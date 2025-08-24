92ed6d9498cdf77c1b49272c11973d3df403bcd6
Subham Roy <145918154+subhamcoder44@users.noreply.github.com>
2025-08-25 00:12:17 +0530

    Update index.c

--- index.c
+++ index.c
@@ -1,5 +1,38 @@
-#include<stdio.h>
-int main(){
-    printf("hello");
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *next; 
+};
+void insertcheck(struct Node *pre)
+{
+    while (pre != NULL)
+    {
+        printf(" element is  %d \n",pre->data);
+        pre=pre->next;
+    }
+}
+
+
+int main() 
+{
+    struct Node *head;
+    struct Node *second ;
+    struct Node *third;
+    head =(struct Node  * )malloc(sizeof(struct Node));
+    second =(struct Node  * )malloc(sizeof(struct Node));
+    third =(struct Node  * )malloc(sizeof(struct Node));
+    head ->data =10;
+    head ->next=second;
+    second -> data=20;
+    second -> next=third;
+    third -> data=30;
+    third -> next = NULL;
+
+     insertcheck(head);
+
+   
     return 0;
+
 }
